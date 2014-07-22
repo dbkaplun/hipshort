c5c40bad1d7c51d2d54a800e1e642043189e3aae
Dan Kaplun <dan@beardtree.com>
2014-07-21 21:57:44 -0500

    `sails generate api URL`

---
+++ api/controllers/URLController.js
@@ -0,0 +1,11 @@
+/**
+ * URLController
+ *
+ * @description :: Server-side logic for managing URLS
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
